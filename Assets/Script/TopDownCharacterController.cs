e9cdd0fd35c909796bb1c7b132781a0a7e9f64a0
Uglyrat695 <sawyepho000@alpinesd.org>
2026-04-29 08:42:56 -0600

    Player

--- Assets/Script/TopDownCharacterController.cs
+++ Assets/Script/TopDownCharacterController.cs
@@ -1,22 +1,26 @@
+using UnityEngine.InputSystem;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using TMPro;
 
-public class TopDownCharacterController : MonoBehaviour
-    {
-        public float speed;
-        private Vector2 moveInput;
-        private Vector2 lookInput; 
-        private Rigidbody2D rigi;
 
-        void FixedUpdate() {
-            if (moveInput.magnitude > 0.1f)
-            {
-                Vector2 moveForward = moveInput.y * this.transform.up;
-                Vector2 moveRight = moveInput.x * this.transform.right;
-                Vector2 moveVector = moveForward + moveRight;
-    
-                rigi.AddForce(moveVector * speed * Time.deltaTime);
-            }
-        }
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    void Update() {
+
+            // Replace movement.x = Input.GetAxisRaw("Horizontal");
+            movement.x = Keyboard.current.dKey.isPressed ? 1 : (Keyboard.current.aKey.isPressed ? -1 : 0);
+
+            // Replace movement.y = Input.GetAxisRaw("Vertical");
+            movement.y = Keyboard.current.wKey.isPressed ? 1 : (Keyboard.current.sKey.isPressed ? -1 : 0);
+       
+        // Normalize the vector so diagonal movement isn't faster
+        movement = movement.normalized;
     }
+
+    void FixedUpdate() {
+        // Move the player using physics
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
